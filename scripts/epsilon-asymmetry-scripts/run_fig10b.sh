35695c4145ada930368db8cb894906add8d3dd23
Huang Wentao <huangwentao@u.nus.edu>
2023-11-01 09:18:30 +0800

    update run_fig10b.sh

--- scripts/epsilon-asymmetry-scripts/run_fig10b.sh
+++ scripts/epsilon-asymmetry-scripts/run_fig10b.sh
@@ -2,6 +2,9 @@ date
 start_time=$(date +%s)
 
 
+# Free up the dataset space for other workloads
+rm -rf /pmemfs0/hashjoin-scm/data
+rm -rf /pmemfs1/hashjoin-scm/data
 
 bash run_fndly.sh > run_fndly.log
 
